194282b4d3d386f3671216f4f29d0abbef5e92de
MMC <manuel.moraleschaves@ucr.ac.cr>
2022-07-25 18:36:27 -0600

    Se divide el main en subrutinas

--- tareas/tetris_solver_mpi/src/tetris_solver_mpi.c
+++ tareas/tetris_solver_mpi/src/tetris_solver_mpi.c
@@ -17,6 +17,100 @@
 #include "tetris_solve.h"
 
 
+/**
+ * @brief Rutina para validar los argumentos de entrada.
+ * @details Valida el archivo inicial y la cantidad de hilos.
+ * @param argc Cantidad de argumentos.
+ * @param argv Argumentos enviados por consola.
+ * @param file_name Nombre del archivo inicial de tetris.
+ * @param thread_count Cantidad de hilos.
+ * @param rank Número del proceso actual.
+ * @return void.
+ */
+void check_params(int argc, char** argv, char file_name[100], int* thread_count,
+                  int rank) {
+    if (argc == 2) {
+        if (sscanf(argv[1], "%s", file_name) != 1 || errno) {
+            if (rank <= 0) {
+                fprintf(stderr, "Error: invalid file name.\n");
+            }
+            exit(1);
+        }
+    } else if (argc == 3) {
+        if (sscanf(argv[1], "%s", file_name) != 1 || errno) {
+            if (rank <= 0) {
+                fprintf(stderr, "Error: invalid file name.\n");
+            }
+            exit(2);
+        }
+
+        if (sscanf(argv[2], "%d", thread_count) != 1
+            || errno) {
+            if (rank <= 0) {
+                fprintf(stderr, "Error: invalid thread count\n");
+            }
+            exit(3);
+        }
+    } else if (argc > 3) {
+        if (rank <= 0) {
+            printf("Bad parameters. Required format: ");
+            printf("./tetris_solver_mpi file_name thread_count\n");
+        }
+        exit(4);
+    }
+}
+
+/**
+ * @brief Rutina para iniciar el procesamiento de la solución.
+ * @details Realiza tareas entre los procesos e inicia el cálculo del tetris.
+ * @param tetris Puntero al estado inicial del tetris.
+ * @param thread_count Cantidad de hilos.
+ * @param rank Número del proceso actual.
+ * @param process_count Cantidad de procesos totales.
+ * @return void.
+ */
+void start_process(tetris_t* tetris, int thread_count, int rank,
+                   int process_count) {
+    // Inicio del conteo del tiempo de ejecución
+    const double startTime = MPI_Wtime();
+
+    if (rank <= 0) {
+        printf("\nResolviendo tetris...\n");
+    }
+    // Inicio de rutina para la solución del tetris
+    solve_tetris_mpi(tetris, thread_count, rank, process_count);
+
+    int i_got_min = 0;
+    int min_height;
+
+    // Se obtiene la altura mínima entre todos los procesos
+    MPI_Allreduce(&tetris->min_height, &min_height, 1, MPI_INT,
+                    MPI_MIN, MPI_COMM_WORLD);
+
+    // En caso de que varios procesos tengan la misma altura mínima
+    // (mejor puntaje), se escoge el de menor rank
+    if (tetris->min_height == min_height) {
+        int min_rank;
+        MPI_Allreduce(&rank, &min_rank, 1, MPI_INT, MPI_MIN,
+                        MPI_COMM_WORLD);
+
+        if (rank == min_rank) {
+            i_got_min = 1;
+        }
+    }
+
+    // Si solo hay 1 proceso o si el proceso actual obtuvo el mejor
+    // puntaje, se crean los archivos y se imprime el tiempo
+    if (process_count <= 1 || i_got_min == 1) {
+        // Generación de archivos finales
+        generate_files(tetris, tetris->levels);
+
+        // Cálculo del tiempo de ejecución
+        double elapsed = MPI_Wtime() - startTime;
+        printf("\nTiempo de ejecución: %.9lfs\n", elapsed);
+    }
+}
+
 int main(int argc, char** argv) {
     // Inicializar MPI
     if (MPI_Init(&argc, &argv) == MPI_SUCCESS) {
@@ -31,35 +125,7 @@ int main(int argc, char** argv) {
         int thread_count = processor_count;
 
         // Validación de parámetros
-        if (argc == 2) {
-            if (sscanf(argv[1], "%s", file_name) != 1 || errno) {
-                if (rank <= 0) {
-                    fprintf(stderr, "Error: invalid file name.\n");
-                }
-                return 1;
-            }
-        } else if (argc == 3) {
-            if (sscanf(argv[1], "%s", file_name) != 1 || errno) {
-                if (rank <= 0) {
-                    fprintf(stderr, "Error: invalid file name.\n");
-                }
-                return 2;
-            }
-
-            if (sscanf(argv[2], "%d", &thread_count) != 1
-                || errno) {
-                if (rank <= 0) {
-                    fprintf(stderr, "Error: invalid thread count\n");
-                }
-                return 3;
-            }
-        } else if (argc > 3) {
-            if (rank <= 0) {
-                printf("Bad parameters. Required format: ");
-                printf("tetris_solver_omp file_name thread_count\n");
-            }
-            return 4;
-        }
+        check_params(argc, argv, file_name, &thread_count, rank);
 
         // Archivo inicial del tetris
         if (rank <= 0) {
@@ -78,6 +144,7 @@ int main(int argc, char** argv) {
                 return 6;
             }
 
+            // Mensajes informativos sobre la cantidad de hilos
             int moves_count = get_tetris_figure_num_rotations(
                                 tetris->figure_sequence[0]) * tetris->columns;
             if (thread_count > moves_count) {
@@ -86,7 +153,6 @@ int main(int argc, char** argv) {
                 }
                 thread_count = processor_count;
             }
-
             if (rank <= 0) {
                 if (process_count > 1) {
                     printf("Number of Threads per process: %d\n", thread_count);
@@ -95,44 +161,8 @@ int main(int argc, char** argv) {
                 }
             }
 
-            // Inicio del conteo del tiempo de ejecución
-            const double startTime = MPI_Wtime();
-
-            if (rank <= 0) {
-                // Inicio de rutina para la solución del tetris
-                printf("\nResolviendo tetris...\n");
-            }
-            solve_tetris_mpi(tetris, thread_count, rank, process_count);
-
-            int i_got_min = 0;
-            int min_height;
-
-            // Se obtiene la altura mínima entre todos los procesos
-            MPI_Allreduce(&tetris->min_height, &min_height, 1, MPI_INT,
-                          MPI_MIN, MPI_COMM_WORLD);
-
-            // En caso de que varios procesos tengan la misma altura mínima
-            // (mejor puntaje), se escoge el de menor rank
-            if (tetris->min_height == min_height) {
-                int min_rank;
-                MPI_Allreduce(&rank, &min_rank, 1, MPI_INT, MPI_MIN,
-                              MPI_COMM_WORLD);
-
-                if (rank == min_rank) {
-                    i_got_min = 1;
-                }
-            }
-
-            // Si solo hay 1 proceso o si el proceso actual obtuvo el mejor
-            // puntaje, se crean los archivos y se imprime el tiempo
-            if (process_count <= 1 || i_got_min == 1) {
-                // Generación de archivos finales
-                generate_files(tetris, tetris->levels);
-
-                // Cálculo del tiempo de ejecución
-                double elapsed = MPI_Wtime() - startTime;
-                printf("\nTiempo de ejecución: %.9lfs\n", elapsed);
-            }
+            // Iniciar el proceso
+            start_process(tetris, thread_count, rank, process_count);
 
             // Liberación de memoria
             destroy_tetris(tetris);
